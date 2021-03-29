c252390e91119d4449aa2590347f9f0587615af9
Gabriel Pereira <gabrielsilva.adm@outlook.com>
2021-03-28 22:33:47 -0300

    Storage implementation

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { StorageService, Item } from '../storage.service';
+import { Platform, ToastController, IonList } from '@ionic/angular';
+// Obs: versões antigas do Ionic, o "IonList" se chama apenas "List".
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,65 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  items: Item[] = [];
 
+  newItem: Item = <Item>{};
+
+  // declarando a lista "mylist" do home-page.html
+  @ViewChild('mylist')mylist: IonList;
+
+  constructor(private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(() => {
+      this.loadItems(); // quando a plataforma for carregada, carregue todos os Items do Storage.
+    });
+  }
+  
+  // CREATE
+  addItem(){
+    this.newItem.modified = Date.now();
+    this.newItem.id = Date.now();
+
+    this.storageService.addItem(this.newItem).then(item => {
+      this.newItem = <Item>{};
+      this.showToast('Item added!');
+      this.loadItems(); // Ou somente adicione no array
+    });
+  }
+
+  // READ
+  loadItems(){
+    this.storageService.getItems().then(items => {
+      this.items = items;
+    });
+  }
+
+  // UPDATE
+  updateItem(item: Item) {
+    item.title = `UPDATED: ${item.title}`;
+    item.modified = Date.now();
+
+    this.storageService.updateItem(item).then(item => {
+      this.showToast('Item updated!');
+      this.mylist.closeSlidingItems();
+      this.loadItems(); // Ou somente atualize no array
+    });
+  } 
+
+  // DELETE
+  deleteItem(item: Item){
+    this.storageService.deleteItem(item.id).then(item => {
+      this.showToast('Item removed!');
+      this.mylist.closeSlidingItems();
+      this.loadItems();
+    });
+  }
+
+  // Helper (visualização do toastController)
+  async showToast(msg){
+    const toast = await this.toastController.create({
+      message: msg,
+      duration: 2000
+    });
+    toast.present();
+  }
 }
